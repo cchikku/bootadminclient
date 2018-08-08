9804c9aa4fc24257c3b22a9a2f2fa80492b34bea
cchikku <chicku.al@gmail.com>
2018-08-08 16:37:27 +0530

    Initial commit

---
+++ src/main/java/com/tis/bootadminclient1/Bootadminclient1Application.java
@@ -0,0 +1,12 @@
+package com.tis.bootadminclient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootadminclient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootadminclient1Application.class, args);
+	}
+}
---
+++ src/test/java/com/tis/bootadminclient1/Bootadminclient1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tis.bootadminclient1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bootadminclient1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
